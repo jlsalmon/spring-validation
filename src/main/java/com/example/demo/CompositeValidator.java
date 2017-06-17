bebb39dba3cef6e91a6d1e42caf73ab96984136c
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2017-06-17 17:33:20 +1000

    Refactoring

--- src/main/java/com/example/demo/CompositeValidator.java
+++ src/main/java/com/example/demo/CompositeValidator.java
@@ -1,15 +1,21 @@
 package com.example.demo;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.support.converter.MessageConversionException;
 import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.Validator;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 /**
  * @author Justin Lewis Salmon
  */
+@Slf4j
 public class CompositeValidator implements Validator {
 
     private List<Validator> validators = new ArrayList<>();
@@ -43,5 +49,23 @@ public class CompositeValidator implements Validator {
 
     public void addValidator(Validator validator) {
         this.validators.add(validator);
+    }
+
+    static class ErrorUtils {
+        static List<DemoApplication.Error> transformErrors(List<FieldError> fieldErrors) {
+            List<DemoApplication.Error> errors = new ArrayList<>();
+
+            for (FieldError error : fieldErrors) {
+                errors.add(new DemoApplication.Error(
+                    error.getObjectName() + "." + error.getField() + "." + error.getCode(),
+                    error.getDefaultMessage(),
+                    error.getField(),
+                    error.getObjectName(),
+                    error.getRejectedValue()
+                ));
+            }
+
+            return errors;
+        }
     }
 }
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,19 +1,19 @@
 package com.example.demo;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.listener.ConditionalRejectingErrorHandler;
-import org.springframework.amqp.rabbit.listener.FatalExceptionStrategy;
 import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
-import org.springframework.amqp.rabbit.listener.exception.ListenerExecutionFailedException;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.amqp.support.converter.MessageConversionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -35,8 +35,6 @@ import org.springframework.web.context.request.RequestAttributes;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -45,67 +43,28 @@ import java.util.Map;
 @ControllerAdvice
 @EnableRabbit
 @SpringBootApplication
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class DemoApplication implements RabbitListenerConfigurer {
-
-    @Autowired
-    private LocalValidatorFactoryBean validatorFactoryBean;
-
-    @Autowired
-    private ApplicationContext context;
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    @RequestMapping("/test")
-    public ResponseEntity test(@Valid @RequestBody Pojo pojo) {
-        return ResponseEntity.ok(pojo);
+    @Data
+    static class Pojo {
+        @NotNull
+        Integer id;
+        @NotNull
+        @Size(min = 2, max = 2)
+        String name;
     }
 
-    @RabbitListener(queues = "test")
-    public void listener(@Valid @Payload Pojo pojo) {
-        log.info(pojo.toString());
-    }
 
-    @Bean
-    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
-    }
 
-    @InitBinder
-    public void initBinder(WebDataBinder binder) {
-        binder.setValidator(compositeValidator());
-    }
-
-    @Bean
-    public ErrorAttributes errorAttributes() {
-        return new MyErrorAttributes();
-    }
-
-    @Override
-    public void configureRabbitListeners(RabbitListenerEndpointRegistrar registrar) {
-        registrar.setMessageHandlerMethodFactory(validatingHandlerMethodFactory());
-    }
-
-    @Bean
-    private DefaultMessageHandlerMethodFactory validatingHandlerMethodFactory() {
-        DefaultMessageHandlerMethodFactory factory =
-            new DefaultMessageHandlerMethodFactory();
-        factory.setValidator(compositeValidator());
-        return factory;
-    }
-
-    @Bean
-    private SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
-        SimpleRabbitListenerContainerFactory listenerContainerFactory =
-            new SimpleRabbitListenerContainerFactory();
-        listenerContainerFactory.setConnectionFactory(connectionFactory);
-        listenerContainerFactory.setErrorHandler(
-            new ConditionalRejectingErrorHandler(
-                new InvalidPayloadRejectingFatalExceptionStrategy()));
-        listenerContainerFactory.setMessageConverter(jackson2JsonMessageConverter());
-        return listenerContainerFactory;
-    }
+    // Generic validation stuff, register all validators for use with
+    // web and amqp requests
+    private final LocalValidatorFactoryBean validatorFactoryBean;
+    private final ApplicationContext context;
 
     @Bean
     public Validator compositeValidator() {
@@ -121,34 +80,36 @@ public class DemoApplication implements RabbitListenerConfigurer {
         return composite;
     }
 
-    /**
-     * Extension of Spring-AMQP's
-     * {@link ConditionalRejectingErrorHandler.DefaultExceptionStrategy}
-     * which also considers a root cause of {@link MethodArgumentNotValidException}
-     * (thrown when payload does not validate) as fatal.
-     */
-    @Slf4j
-    static class InvalidPayloadRejectingFatalExceptionStrategy implements FatalExceptionStrategy {
-
-        @Override
-        public boolean isFatal(Throwable t) {
-            if (t instanceof ListenerExecutionFailedException &&
-                (t.getCause() instanceof MessageConversionException || t.getCause() instanceof MethodArgumentNotValidException)) {
-                log.warn("Fatal message conversion error; message rejected; it will be dropped: {}",
-                    ((ListenerExecutionFailedException) t).getFailedMessage());
-                return true;
-            }
-            return false;
-        }
+    @Data
+    @AllArgsConstructor
+    public static class Error {
+        private String code;
+        private String message;
+        private String field;
+        private String objectName;
+        private Object rejectedValue;
     }
 
-    @Data
-    static class Pojo {
-        @NotNull
-        Integer id;
-        @NotNull
-        @Size(min = 2, max = 2)
-        String name;
+
+
+
+    // Web
+    @RequestMapping("/test")
+    public ResponseEntity test(@Valid @RequestBody Pojo pojo) {
+        return ResponseEntity.ok(pojo);
+    }
+
+    // Register composite validator with webmvc validation support
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.setValidator(compositeValidator());
+    }
+
+    // Override the default error attributes to remove some of the
+    // unnecessary stuff
+    @Bean
+    public ErrorAttributes errorAttributes() {
+        return new MyErrorAttributes();
     }
 
     static class MyErrorAttributes extends DefaultErrorAttributes {
@@ -159,19 +120,8 @@ public class DemoApplication implements RabbitListenerConfigurer {
             attributes.remove("exception");
 
             if (attributes.containsKey("errors") && getError(requestAttributes) instanceof org.springframework.web.bind.MethodArgumentNotValidException) {
-                Collection<Error> errors = new ArrayList<>();
-
-                for (FieldError error : (List<FieldError>) attributes.get("errors")) {
-                    errors.add(new Error(
-                        error.getObjectName() + "." + error.getField() + "." + error.getCode(),
-                        error.getDefaultMessage(),
-                        error.getField(),
-                        error.getObjectName(),
-                        error.getRejectedValue()
-                    ));
-                }
-
-                attributes.put("errors", errors);
+                attributes.put("errors", CompositeValidator.ErrorUtils
+                    .transformErrors((List<FieldError>) attributes.get("errors")));
             }
 
             return attributes;
@@ -183,13 +133,57 @@ public class DemoApplication implements RabbitListenerConfigurer {
         }
     }
 
-    @Data
-    @AllArgsConstructor
-    public static class Error {
-        private String code;
-        private String message;
-        private String field;
-        private String objectName;
-        private Object rejectedValue;
+
+
+    // AMQP
+    @RabbitListener(queues = "test")
+    public void listener(@Valid @Payload Pojo pojo) {
+        log.info(pojo.toString());
+    }
+
+    // Enable JSON
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    // Register composite validator with AMQP infrastructure
+    @Override
+    public void configureRabbitListeners(RabbitListenerEndpointRegistrar registrar) {
+        registrar.setMessageHandlerMethodFactory(validatingHandlerMethodFactory());
+    }
+
+    @Bean
+    public DefaultMessageHandlerMethodFactory validatingHandlerMethodFactory() {
+        DefaultMessageHandlerMethodFactory factory = new DefaultMessageHandlerMethodFactory();
+        factory.setValidator(compositeValidator());
+        return factory;
+    }
+
+    // Register custom advice chain to log error messages
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(jackson2JsonMessageConverter());
+        factory.setAdviceChain(new ExceptionLoggingAdvice());
+        return factory;
+    }
+
+    class ExceptionLoggingAdvice implements MethodInterceptor {
+
+        @Override
+        public Object invoke(MethodInvocation invocation) throws Throwable {
+            try {
+                return invocation.proceed();
+            } catch (Exception e) {
+                if (e.getCause() instanceof MethodArgumentNotValidException) {
+                    log.warn(new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(
+                        CompositeValidator.ErrorUtils.transformErrors(((MethodArgumentNotValidException) e.getCause()).getBindingResult().getFieldErrors())));
+                }
+
+                throw e;
+            }
+        }
     }
 }
